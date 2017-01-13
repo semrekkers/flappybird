de2b0b64bed4cb74ba2ef2f109357c8b08a51b47
janlindenberg <jan02_03@hotmail.com>
2017-01-13 13:47:04 +0100

    update GemeThread

--- app/src/main/java/com/hertogsem/flappybird/GameThread.java
+++ app/src/main/java/com/hertogsem/flappybird/GameThread.java
@@ -1,8 +1,44 @@
 package com.hertogsem.flappybird;
 
+import android.graphics.Canvas;
+import android.util.Log;
+
 /**
  * Created by janlindenberg on 13/01/2017.
  */
 
-public class GameThread {
+public class GameThread extends Thread{
+
+    private GameSurfaceView surfaceView;
+    private Boolean isRunning;
+
+    public GameThread(GameSurfaceView view) {
+        this.surfaceView = view;
+        this.isRunning = false;
+    }
+
+    public void running(boolean running) {
+        this.isRunning = running;
+    }
+
+    @Override
+    public void run() {
+        while(isRunning) {
+
+            // Update canvas
+            Canvas canvas = surfaceView.getHolder().lockCanvas();
+            if( canvas != null ) {
+                synchronized (surfaceView.getHolder()) {
+                    surfaceView.updateGame(canvas);
+                }
+                surfaceView.getHolder().unlockCanvasAndPost(canvas);
+            }
+
+            // Sleep
+            try { sleep(1000/60); } catch (InterruptedException ex) {
+                Log.e("", ex.getLocalizedMessage());
+            }
+        }
+    }
+
 }
