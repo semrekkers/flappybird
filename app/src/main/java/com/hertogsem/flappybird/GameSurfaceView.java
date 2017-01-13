3ff4cd0dbcf19e938be2bcf94d51bca1acec03fc
Sem Rekkers <sem@devments.com>
2017-01-13 14:15:03 +0100

    Snapshot GameSurfaceView

--- app/src/main/java/com/hertogsem/flappybird/GameSurfaceView.java
+++ app/src/main/java/com/hertogsem/flappybird/GameSurfaceView.java
@@ -2,12 +2,17 @@ package com.hertogsem.flappybird;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class GameSurfaceView extends SurfaceView {
+    private static final String TAG = "GameSurfaceView";
 
     private GameThread thread;
+    private SurfaceHolder holder;
 
     public GameSurfaceView(Context context) {
         super(context);
@@ -25,11 +30,31 @@ public class GameSurfaceView extends SurfaceView {
     }
 
     private void init() {
+        this.thread = new GameThread(this);
+        this.holder = getHolder();
 
+        this.paint = new Paint();
+        try {
+            this.background = new Background(getContext());
+        }
+        catch (Exception ex) {
+            Log.e(TAG, "Exception on constructing Background: "+ex.getMessage());
+        }
     }
 
-    protected void updateGame(Canvas canvas) {
+    // NOT THREAD SAFE
+    private Background background;
+    private Paint paint;
+    // END OF NOT THREAD SAFE
 
+    protected void updateGame(Canvas canvas) {
+        long time = System.currentTimeMillis();
+        int width = canvas.getWidth();
+        int height = canvas.getHeight();
+
+        // Only for testing
+        background.update(time, width, height);
+        background.draw(canvas, paint);
     }
 
 }
